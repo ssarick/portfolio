624f776c495dc8a036a2a26e5ef5883a597245ee
ssarick <66787805+ssarick@users.noreply.github.com>
2021-04-23 16:27:16 +0500

    hey changes

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggle() {
+    var sec = document.getElementById('sec');
+    sec.classList.toggle('active')
+    var nav = document.getElementById('navigation');
+    nav.classList.toggle('active')
+}
